616e49b7dbd1480a48d3cee439050b5ea314a737
Abhisheksah12 <aarav12sah@gmail.com>
2023-07-26 15:29:00 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { ProductList } from "../features/product-list/ProductList";
+import Navbar from "../features/navbar/Navbar";
+
+
+function Home() {
+  return (
+    <div>
+     
+    <Navbar>
+      <ProductList></ProductList>
+    </Navbar>
+   
+     
+      
+       
+     
+       
+     
+    </div>
+  );
+}
+
+export default Home;
