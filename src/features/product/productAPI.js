0405cc433576d102d32161b37b9a5f6e98f01998
Abhisheksah12 <aarav12sah@gmail.com>
2023-07-28 17:09:18 +0530

    Filter and sort changes

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,24 +1,33 @@
 export function fetchAllProducts() {
-  return new Promise(async(resolve) =>{
+  return new Promise(async (resolve) => {
     // todo: we will not hard code server url here
-   const respone = await fetch('http://localhost:8080/products')
-   const data = await respone.json()
-   resolve ({data})
-  }
-  );
+    const respone = await fetch("http://localhost:8080/products");
+    const data = await respone.json();
+    resolve({ data });
+  });
 }
 
-export function fetchProductsByFilters(filter) {
-
-let queryString ="";
-  for(let key in filter){
-      queryString+=`${key}=${filter[key]}&`
+export function fetchProductsByFilters(filter, sort) {
+  //filter ={"category":["smartphone","laptop"]}
+  //sort={_sort:"price",_order="desc"}
+  //TODO : on server we will support multiple values
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
   }
-  return new Promise(async(resolve) =>{
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+  }
+  return new Promise(async (resolve) => {
     // todo: we will not hard code server url here
-   const respone = await fetch('http://localhost:8080/products?'+queryString)
-   const data = await respone.json()
-   resolve ({data})
-  }
-  );
+    const respone = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await respone.json();
+    resolve({ data });
+  });
 }
