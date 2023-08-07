af4c5508926d9a73e316df503d788c3f45733faf
Abhisheksah12 <aarav12sah@gmail.com>
2023-08-07 15:46:58 +0530

    Login/Signup Added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,84 @@
-import { Counter } from './features/counter/Counter';
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './features/auth/components/Signup';
+import { Counter } from "./features/counter/Counter";
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./features/auth/components/Signup";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from "react-router-dom";
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
-import ProductDetailsPage from './pages/ProductDetailsPage';
-
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import Protected from "./features/auth/components/Protected";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <div> <Home></Home></div>
+      <div>
+        {" "}
+        <Protected>
+          <Home></Home>
+        </Protected>
+      </div>
     ),
   },
   {
     path: "/login",
-    element: <div><LoginPage></LoginPage></div>,
+    element: (
+      <div>
+        <LoginPage></LoginPage>
+      </div>
+    ),
   },
   {
     path: "/signup",
-    element: <div><SignupPage></SignupPage></div>,
+    element: (
+      <div>
+        <SignupPage></SignupPage>
+      </div>
+    ),
   },
   {
     path: "/cart",
-    element: <div><CartPage></CartPage></div>,
+    element: (
+      <div>
+        <Protected>
+          <CartPage></CartPage>
+        </Protected>
+      </div>
+    ),
   },
   {
     path: "/checkout",
-    element: <div><Checkout></Checkout></div>,
+    element: (
+      <div>
+        <Protected>
+          <Checkout></Checkout>
+        </Protected>
+      </div>
+    ),
   },
   {
     path: "/product-detail/:id",
-    element: <div><ProductDetailsPage></ProductDetailsPage></div>,
+    element: (
+      <div>
+        <Protected>
+          <ProductDetailsPage></ProductDetailsPage>
+        </Protected>
+      </div>
+    ),
   },
 ]);
-
 
 function App() {
   return (
     <div className="App">
-     <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
